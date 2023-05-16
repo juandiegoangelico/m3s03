065261605de0942db78910fd7c0e0430bb8fc907
Juan Diego Angélico <juandiegoadv@gmail.com>
2023-05-16 20:04:57 -0300

    first commit

---
+++ src/main/java/br/com/lagoinha/m3s03/M3s03Application.java
@@ -0,0 +1,13 @@
+package br.com.lagoinha.m3s03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M3s03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M3s03Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lagoinha/m3s03/M3s03ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lagoinha.m3s03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M3s03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
